b9e51f02e4a5df14d7504a2a46d14f8ec3e7b6c9
ebra <ebrimasonko188@gmail.com>
2022-04-23 01:55:31 +0200

    modified files seachParams and added pet.js & Result.js files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { render } from "react-dom";
+import { StrictMode } from "react/cjs/react.production.min";
 import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
-    <div>
+    <StrictMode>
+      <div>
       <h1>Adopt Me!</h1>
       <SearchParams />
     </div>
+    </StrictMode>
   );
 };
 
